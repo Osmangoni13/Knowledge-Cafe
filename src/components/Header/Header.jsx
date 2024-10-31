c464c316152da64bb25807e188b44a08c6f3b95e
Osmangoni13 <osmanmonju@gmail.com>
2024-10-31 19:45:37 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-center font-bold text-2xl py-4 ">Header</h1>
+        </div>
+    );
+};
+
+export default Header;
